a24eda89e1df549389fdde675d938c21084aaf40
TGPstudent <tarasenko.student@gmail.com>
2023-06-04 22:14:44 +0300

    Добавьте файлы проекта.

---
+++ TSPP_Lab_4_2_2023/FilmLibrary.cs
@@ -0,0 +1,227 @@
+﻿using System;
+
+namespace TSPP_Lab_4_2_2023
+{
+    class FilmLibrary
+    {
+        private string name;
+        private string last_name;
+        private int year;
+        private string studio;
+        private DateTime time;
+        public FilmLibrary() //Конструктор без параметрів
+        { }
+
+        //Конструктор з параметрами
+        public FilmLibrary(string Str)
+        {
+            string[] row = Str.Split(new char[] { ';' });
+            Name = row[0];
+            Last_name = row[1];
+            Year = row[2];
+            Studio = row[3];
+            Time = row[4];
+        }
+        //Методи:
+        //Виведення бази в консоль
+        static public void Print(FilmLibrary[] fbas, int n)
+        {
+            Console.WriteLine("  __________________________________________________________________________________________________________________________");
+            Console.WriteLine(" |   №\t|  Назва фільму\t\t\t|  Прізвище режисера\t|  Рік виходу\t|    Кіностудія\t\t\t| Тривалість фільму |");
+            Console.WriteLine(" |______|_______________________________|_______________________|_______________|_______________________|___________________|");
+            if (fbas.Length > 1)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(String.Format($" | {i+1,-4} |{fbas[i].Name, -30} |  {fbas[i].Last_name,-21}|  {fbas[i].Year, -13}|  {fbas[i].Studio, -21}|  {fbas[i].Time, -17}|"));
+                }
+                Console.WriteLine(" |______|_______________________________|_______________________|_______________|_______________________|___________________|");
+            }
+            else Console.WriteLine("Записи в базі відсутні");
+        }
+        //Додавання запису
+        public FilmLibrary Inp ()
+        {
+            FilmLibrary inp = new FilmLibrary();
+            Console.Write("Введіть Назву фільму: ");
+            inp.Name = Console.ReadLine();
+            Console.Write("Введіть прізвище режисера: ");
+            inp.Last_name = Console.ReadLine();
+            Console.Write("Введіть рік випуску картини: ");
+            inp.Year = Console.ReadLine();
+            Console.Write("Введіть назву студії: ");
+            inp.Studio = Console.ReadLine();
+            Console.Write("Введіть час тривалостіі фільму у форматі (ГГ:ХХ:СС): ");
+            inp.Time = Console.ReadLine();
+            return inp;
+        }
+        //Рeдагування полів запису з обранням потрібного
+        public FilmLibrary Correct_Bas (FilmLibrary _inp)
+        {
+            FilmLibrary inp = new FilmLibrary();
+            inp = _inp;
+            int N;
+            do
+            {
+                Console.WriteLine(" Оберіть поле, яке потребує коригування: ");
+                Console.WriteLine(" Натисність 1 - для редагування назви фільму");
+                Console.WriteLine(" Натисність 2 - для редагування прізвища кінорежисера");
+                Console.WriteLine(" Натисність 3 - для редагування року випуску");
+                Console.WriteLine(" Натисність 4 - для редагування студії");
+                Console.WriteLine(" Натисність 5 - для для редагування тривалості картини");
+                Console.WriteLine(" Натисність Enter - для закінчення редагуванн цього запису");
+                string p = Console.ReadLine();
+                if (p.Trim() != "") N = int.Parse(p);
+                else N = 0;
+                switch (N)
+                {
+                    case 1:
+                        {
+                            Console.Write("Введіть Назву фільму: ");
+                            inp.Name = Console.ReadLine();
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.Write("Введіть прізвище режисера: ");
+                            inp.Last_name = Console.ReadLine();
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.Write("Введіть рік випуску картини: ");
+                            inp.Year = Console.ReadLine();
+                        }
+                        break;
+                    case 4:
+                        {
+                            Console.Write("Введіть назву студії: ");
+                            inp.Studio = Console.ReadLine();
+                        }
+                        break;
+                    case 5:
+                        {
+                            Console.Write("Введіть час тривалості фільму у форматі (ГГ:ХХ:СС): ");
+                            inp.Time = Console.ReadLine();
+                        }
+                        break;
+                    default: Console.Write("Редагування запису закінчено"); break;
+                }
+            }
+            while (N != 0);
+         return (inp);
+        }
+        //Редагування запису цілком
+        public FilmLibrary All_Corect(FilmLibrary _inp)
+        {
+            FilmLibrary inp = new FilmLibrary();
+            Console.Write("Введіть Назву фільму: ");
+            inp.Name = Console.ReadLine();
+            Console.Write("Введіть прізвище режисера: ");
+            inp.Last_name = Console.ReadLine();
+            Console.Write("Введіть рік випуску картини: ");
+            inp.Year = Console.ReadLine();
+            Console.Write("Введіть назву студії: ");
+            inp.Studio = Console.ReadLine();
+            Console.Write("Введіть час тривалостіі фільму у форматі (ГГ:ХХ:СС): ");
+            inp.Time = Console.ReadLine();
+            return inp;
+        }
+        //Пошук по року випуску
+        static public void Search (FilmLibrary[] fbas, int _year, int n)
+        {
+            Console.WriteLine("В рік заданий Вами випущено наступні картини:");
+            Console.WriteLine("  ___________________________________________________________________________________________________________________");
+            Console.WriteLine(" |  Назва фільму \t\t |  Прізвище режисера\t |  Рік виходу \t |   Кіностудія\t\t | Тривалість фільму |");
+            Console.WriteLine(" |_______________________________|_______________________|_______________|_______________________|___________________|");
+           for (int i = 0; i < n; i++)
+            {
+                if (fbas[i].year == _year)
+                    Console.WriteLine(String.Format($" |{fbas[i].Name,-30} |  {fbas[i].Last_name,-21}|  {fbas[i].Year,-13}|  {fbas[i].Studio,-21}|  {fbas[i].Time,-17}|"));
+            }
+            Console.WriteLine(" |_______________________________|_______________________|_______________|_______________________|___________________|");
+        }
+        //Сортування по полю Name 
+        static public void Sorting(FilmLibrary[] fbas, int n)
+        {
+            FilmLibrary temp = new FilmLibrary();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (string.Compare(fbas[i].name, fbas[j].name) < 0)
+                    {
+                        temp = fbas[i];
+                        fbas[i] = fbas[j];
+                        fbas[j] = temp;
+                    }
+                }
+            }
+        }
+        //Сортування строк масиву з видаленням пустих
+        public static int Sort (FilmLibrary[] fbas, int n)
+        {
+            int i;
+            for (i = 0; i < n-1; i++)
+            {
+                if (fbas[i].Name == null && fbas[i + 1].Name != null)
+                {
+                    fbas[i] = fbas[i + 1];
+                    fbas[i + 1] = new FilmLibrary();
+                }
+            }
+            n = i;
+            return n;
+        }
+
+        //Властивості
+        public string Name
+        {
+            get { return name; }
+            set {
+                if (value.Trim() != "")
+                    name = value.Trim();
+                else throw new Exception("Не вдалось считати назву фільму!");
+            }
+        }
+        public string Last_name
+        {
+            get { return last_name; }
+            set
+            {
+                if (value.Trim() != "")
+                    last_name = value.Trim();
+                else throw new Exception ("Не вдалось считати прізвище автора!");
+            }
+        }
+        public string Year
+        {
+            get {return year.ToString(); }
+            set
+            {
+                if (value.Trim() != " " && Convert.ToInt32(value) > 0) year = Convert.ToInt32(value);
+                else throw new Exception("Не вдалось считати рік випуску фільму чи рік задано не коректно!");
+            }
+        }
+        public string Studio
+        {
+            get { return studio; }
+            set
+            {
+                if (value.Trim() != " ")
+                    studio = value.Trim();
+                else throw new Exception("Не вдалось считати назву студії!");
+            }
+        }
+        public string Time
+        {
+            get { return time.ToString("T"); }
+            set
+            {
+                if (value.Trim() != "" && DateTime.TryParse(value, out time) && time != null)
+                 DateTime.TryParse(value, out time);
+                else throw new Exception("Не вдалось считати тривалість стрічки чи час некоректний!");
+            }
+        }
+    }
+}
---
+++ TSPP_Lab_4_2_2023/Program.cs
@@ -0,0 +1,195 @@
+﻿//TSPP_Lab_4_2_2023 Робота з базою данних через меню консолі
+using System;
+using System.Text;
+using System.IO;
+
+namespace TSPP_Lab_4_2_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            string pathInpFile = ("E:\\BUK_UNIVER\\II_kurs(II sem)\\ТСПП\\Laboratorni\\Lab4\\TSPP_Lab_4_2_2023\\Basa.txt");
+            if (!File.Exists(pathInpFile))
+            {
+                Console.WriteLine("Файл:\n" + pathInpFile + "\n не можливо відкрити! Перевірьте його існування.");
+                return;
+            }
+            else
+                Console.WriteLine("Файл: ...//Basa.txt успішно відкрито!");
+            
+            string Line;
+            int n=0, i=0;
+            n = File.ReadAllLines(pathInpFile).Length;
+            FilmLibrary[] Fbas = new FilmLibrary[n];
+            try
+            {
+                StreamReader file = new StreamReader(File.Open(pathInpFile, FileMode.Open));
+                while ((Line = file.ReadLine()) != null)
+                {
+                 Fbas[i] = new FilmLibrary(Line);
+                 ++i;
+                }
+
+                if(i == 0) Console.WriteLine("Файл: ...//Basa.txt порожній! Перевірьте та збережіть в ньому текст");
+                n = i;
+                file.Close();
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+               Console.WriteLine("Дуже великий файл!");
+               return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Упс!: " + e.Message);
+                return;
+            }
+            int N;
+            do
+            {
+                Console.WriteLine("\n _________________МEНЮ:__________________________________");
+                Console.WriteLine(" Натисність 1 - для додавання записів");
+                Console.WriteLine(" Натисність 2 - для редагування обраних полів записів");
+                Console.WriteLine(" Натисність 3 - для редагування запису");
+                Console.WriteLine(" Натисність 4 - для знищення записів");
+                Console.WriteLine(" Натисність 5 - для пошук фільмів за заданим роком випуску");
+                Console.WriteLine(" Натисність 6 - для сортування списку за Назвою картини");
+                Console.WriteLine(" Натисність 7 - Вивести базу на екран");
+                Console.WriteLine(" Натисність Enter - для виходу з програми");
+                Console.WriteLine(" __________________________________________________________");
+                string sws = Console.ReadLine();
+                if (sws.Trim() != "") N = int.Parse(sws);
+                else N = 0;
+                switch (N)
+                {
+                    case 1:
+                        {//додавання записів
+                            Console.WriteLine("Введіть інформацію про фільм для внесення його в базу:");
+                            Array.Resize(ref Fbas, ++n);
+                            FilmLibrary temp = new FilmLibrary();
+                            Fbas[n-1] = temp.Inp();
+                        }
+                        break;
+                    case 2:
+                        {//редагування полів запису
+                            string k;
+                            do
+                            {
+                                Console.Clear();
+                                Console.WriteLine("База даних містить наступні записи:");
+                                FilmLibrary.Print(Fbas, n);
+                                Console.Write("\n Оберіть № запису, який потрібно відредагувати:");
+                                int nl = Convert.ToInt32(Console.ReadLine());
+                                FilmLibrary temp = new FilmLibrary();
+                                Fbas[nl-1] = temp.Correct_Bas(Fbas[nl-1]);
+                                Console.Clear();
+                                Console.WriteLine("Скоригована база даних містить наступні записи:");
+                                FilmLibrary.Print(Fbas, n);
+                                Console.WriteLine("Бажаєте відредагувати ще? т-TAK? для закiнчення натисність ENTER");
+                                k = Console.ReadLine();
+                            }
+                            while (k == "т");
+                        }
+                        break;
+
+                    case 3:
+                        {//Редагування запису цілком
+                            string k;
+                            do
+                            {
+                                Console.Clear();
+                                Console.WriteLine("База даних містить наступні записи:");
+                                FilmLibrary.Print(Fbas,n);
+                                Console.Write("\n Оберіть № запису, який потрібно відредагувати:");
+                                int nl = Convert.ToInt32(Console.ReadLine());
+                                FilmLibrary temp = new FilmLibrary();
+                                Fbas[nl-1] = temp.All_Corect(Fbas[nl-1]);
+                                Console.Clear();
+                                Console.WriteLine("Скоригована база даних містить наступні записи:");
+                                FilmLibrary.Print(Fbas, n);
+                                Console.WriteLine("Бажаєте відредагувати ще? т-TAK? для закінчення натисність ENTER");
+                                k = Console.ReadLine();
+                            }
+                            while (k == "т");
+                        }
+                        break;
+                    case 4:
+                        {//знищення записів
+                            string k;
+                            do
+                            {
+                                Console.Clear();
+                                Console.WriteLine("База даних містить наступні записи:");
+                                FilmLibrary.Print(Fbas, n);
+                                Console.Write("Оберіть № запису, який потрібно видалити:");
+                                int nl = Convert.ToInt32(Console.ReadLine());
+                                Fbas[nl-1] = new FilmLibrary();
+                                n = FilmLibrary.Sort(Fbas,n);
+                                Console.Clear();
+                                Console.WriteLine("Оновлена база даних містить наступні записи:");
+                                FilmLibrary.Print(Fbas, n);
+                                Console.WriteLine("Бажаєте вилучити ще один запис? т-TAK? для закінчення натисність ENTER");
+                                k = Console.ReadLine();
+                            }
+                            while (k == "т");
+                            
+                        }
+                        break;
+                    case 5:
+                        {//пошук записів по полю year
+                            Console.Clear();
+                            Console.WriteLine("База даних містить наступні записи:");
+                            FilmLibrary.Print(Fbas,n);
+                            Console.Write("Введіть рік виходу фільму для пошуку: ");
+                            int yaer = Convert.ToInt32(Console.ReadLine());
+                            FilmLibrary.Search(Fbas, yaer, n);
+                        }
+                        break;
+
+                    case 6:
+                        {//сортування записів по полю name
+                            Console.Clear();
+                            FilmLibrary.Sorting(Fbas,n);
+                            Console.WriteLine("База даних відсортована за алфавітом по полю назви фільму:");
+                            FilmLibrary.Print(Fbas,n);
+                        }
+                        break;
+
+                    case 7:
+                        {//виведення на екран
+                            Console.Clear();
+                            FilmLibrary.Print(Fbas,n);
+                        }
+                        break;
+
+                    default:
+                        {//вихід з програми
+                            Console.Clear();
+                            Console.WriteLine("Зараз база даних має наступний вигляд:");
+                            FilmLibrary.Print(Fbas,n);
+                            Console.WriteLine("\n Ви бажаєте зберегти цю інформацію у файл? Натисніть т-(Так), для виходу натисніть ENTER");
+                            string kay = Console.ReadLine(); ;
+                            if (kay == "т")
+                            {
+                                StreamWriter f = new StreamWriter("E:\\BUK_UNIVER\\II_kurs(II sem)\\ТСПП\\Laboratorni\\Lab4\\TSPP_Lab_4_2_2023\\Basa.txt");
+                                for (int j =0; j<n; j++)
+                                {
+                                    f.WriteLine(String.Format($"{Fbas[j].Name}; {Fbas[j].Last_name}; {Fbas[j].Year}; {Fbas[j].Studio}; {Fbas[j].Time}; \n"));
+                                }
+                                f.Close();
+                                Console.WriteLine("Інформацію записано у файл:E:\\BUK_UNIVER\\II_kurs(II sem)\\ТСПП\\Laboratorni\\Lab4\\TSPP_Lab_4_2_2023\\Basa.txt ");
+                            }
+                        }
+                        break;
+                }
+            }
+            while (N != 0);
+            Console.ReadLine();
+        }
+    }
+}
